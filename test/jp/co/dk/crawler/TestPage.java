755e3a7c2e89e976060910abbb4e2ca4ea926fe2
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-10-20 17:06:38 +0000

    クローラ作成中 Pageクラスを実装中
    git-svn-id: file:///opt/svn/repository/projects/trunk@313 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ test/jp/co/dk/crawler/TestPage.java
@@ -0,0 +1,14 @@
+package jp.co.dk.crawler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPage {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

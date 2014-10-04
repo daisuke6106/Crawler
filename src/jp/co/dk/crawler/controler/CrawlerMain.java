0533bd2cc0dad19e9f57814aff4d3427b237cafa
Daisuke Kanno <daisuke6106@gmail.com>
2014-10-05 02:18:45 +0900

    使用していないコントローラを削除した

--- src/jp/co/dk/crawler/controler/CrawlerMain.java
+++ src/jp/co/dk/crawler/controler/CrawlerMain.java
@@ -3,8 +3,11 @@ package jp.co.dk.crawler.controler;
 import java.util.ArrayList;
 import java.util.List;
 
+import jp.co.dk.browzer.Page;
+import jp.co.dk.browzer.exception.BrowzingException;
 import jp.co.dk.browzer.exception.PageAccessException;
 import jp.co.dk.browzer.exception.PageIllegalArgumentException;
+import jp.co.dk.browzer.html.element.MovableElement;
 import jp.co.dk.crawler.Crawler;
 import jp.co.dk.crawler.exception.CrawlerException;
 import jp.co.dk.crawler.exception.CrawlerInitException;
@@ -12,9 +15,13 @@ import jp.co.dk.crawler.exception.CrawlerSaveException;
 import jp.co.dk.datastoremanager.DataStoreManager;
 import jp.co.dk.datastoremanager.exception.DataStoreManagerException;
 import jp.co.dk.datastoremanager.property.DataStoreManagerProperty;
+import jp.co.dk.document.Element;
+import jp.co.dk.document.ElementSelector;
+import jp.co.dk.document.File;
 import jp.co.dk.document.exception.DocumentException;
-import jp.co.dk.logger.Logger;
-import jp.co.dk.logger.LoggerFactory;
+import jp.co.dk.document.html.HtmlDocument;
+import jp.co.dk.document.html.HtmlElement;
+import jp.co.dk.document.html.constant.HtmlElementName;
 import jp.co.dk.property.exception.PropertyException;
 
 import org.apache.commons.cli.CommandLine;
@@ -26,6 +33,10 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
 public class CrawlerMain {
+	
+	protected Options options = new Options();
+	
+	protected CommandLine cmd;
 	
 	/**
 	 * ---OptionBuilder---
@@ -69,17 +80,11 @@ public class CrawlerMain {
 	 * 
 	 * @param args 起動引数
 	 */
-	public static void main(String[] args) {
-		
-		Options options = new Options();
-		List<Option> optionList = new ArrayList<Option>();
-		Option  url = OptionBuilder.isRequired(true).hasArg(true).withArgName("保存対象のURL").withDescription("保存対象のURL").create( "url" );
-		Option  all = OptionBuilder.isRequired(false).hasArg(false).withDescription("そのURLに関するデータをすべて保存するか").create( "all" );
-		options.addOption(url);
-		options.addOption(all);
-		CommandLine cmd = null;
+	public void main(String[] args) {
+		this.options.addOption(OptionBuilder.isRequired(true).hasArg(true).withArgName("保存対象のURL").withDescription("保存対象のURL").create("url"));
+		this.options.addOption(OptionBuilder.isRequired(false).hasArg(false).withDescription("そのURLに関するデータをすべて保存するか").create("all"));
 		try {
-			cmd = new PosixParser().parse(options, args);
+			this.cmd = new PosixParser().parse(options, args);
 		} catch (ParseException e) {
 			HelpFormatter help = new HelpFormatter();
 			help.printHelp("crawler", options, true);
@@ -99,7 +104,7 @@ public class CrawlerMain {
 		try {
 			dsm.startTrunsaction();
 			Crawler crawler = new Crawler(cmd.getOptionValue("url"), dsm);
-			if (cmd.hasOption(all.getOpt())) {
+			if (cmd.hasOption("all")) {
 				crawler.saveAll();
 			} else {
  				crawler.save();
@@ -128,4 +133,106 @@ public class CrawlerMain {
 		}
 		
 	}
+	
+	
+	/**
+	 * 指定のページが参照している以下の要素を取得します。<p/>
+	 * <br/>
+	 * 指定のページがHTML以外である場合、空のリストを返却します。<br/>
+	 * <br/>
+	 * ・link  （hrefに値が設定されている要素のみ）<br/>
+	 * ・script（srcに値が設定されている要素のみ）<br/>
+	 * ・image （srcに値が設定されている要素のみ）<br/>
+	 * 
+	 * @param page 要素取得先ページ
+	 * @return 要素一覧
+	 * @throws BrowzingException ページ情報の読み込みに失敗した場合
+	 * @throws DocumentException 
+	 */
+	List<Element> getRefsElements(Page page) throws BrowzingException, DocumentException {
+		List<Element> elementList = new ArrayList<Element>();
+		File file = page.getDocument();
+		if (file instanceof HtmlDocument) return elementList;
+		HtmlDocument htmlDocument = (HtmlDocument)file;
+		List<Element> links   = htmlDocument.getElement(new LinkElementSelector());
+		List<Element> scripts = htmlDocument.getElement(new ScriptElementSelector());
+		List<Element> imgs    = htmlDocument.getElement(new ImageElementSelector());
+		elementList.addAll(links);
+		elementList.addAll(scripts);
+		elementList.addAll(imgs);
+		return elementList;
+	}
+	
+	/**
+	 * 指定のページが参照している以下の要素を取得します。<p/>
+	 * <br/>
+	 * 指定のページがHTML以外である場合、空のリストを返却します。<br/>
+	 * <br/>
+	 * ・anchor（hrefに値が設定されている要素のみ）<br/>
+	 * 
+	 * @param page 要素取得先ページ
+	 * @return 要素一覧
+	 * @throws BrowzingException ページ情報の読み込みに失敗した場合
+	 * @throws DocumentException 
+	 */
+	List<Element> getAnchorElements(Page page) throws BrowzingException, DocumentException {
+		List<Element> elementList = new ArrayList<Element>();
+		File file = page.getDocument();
+		if (file instanceof HtmlDocument) return elementList;
+		HtmlDocument htmlDocument = (HtmlDocument)file;
+		List<Element> anchors = htmlDocument.getElement(new AnchorElementSelector());
+		elementList.addAll(anchors);
+		return elementList;
+	}
+}
+
+class MovableElementSelector implements ElementSelector {
+	@Override
+	public boolean judgment(Element element) {
+		if (element instanceof MovableElement) return true;
+		return false;
+	}
+	
+}
+
+class AnchorElementSelector implements ElementSelector {
+	@Override
+	public boolean judgment(Element element) {
+		if (!(element instanceof HtmlElement)) return false;
+		HtmlElement htmlElement = (HtmlElement)element;
+		if (htmlElement.getElementType() == HtmlElementName.A && htmlElement.hasAttribute("href") && (!htmlElement.getAttribute("href").equals("")) ) return true;
+		return false;
+	}
+	
+}
+
+class LinkElementSelector implements ElementSelector {
+	@Override
+	public boolean judgment(Element element) {
+		if (!(element instanceof HtmlElement)) return false;
+		HtmlElement htmlElement = (HtmlElement)element;
+		if (htmlElement.getElementType() == HtmlElementName.LINK && htmlElement.hasAttribute("href") && (!htmlElement.getAttribute("href").equals("")) ) return true;
+		return false;
+	}
+}
+
+class ScriptElementSelector implements ElementSelector {
+	@Override
+	public boolean judgment(Element element) {
+		if (!(element instanceof HtmlElement)) return false;
+		HtmlElement htmlElement = (HtmlElement)element;
+		if (htmlElement.getElementType() == HtmlElementName.SCRIPT && htmlElement.hasAttribute("src") && (!htmlElement.getAttribute("src").equals(""))) return true;
+		return false;
+	}
+}
+
+class ImageElementSelector implements ElementSelector {
+	@Override
+	public boolean judgment(Element element) {
+		if (!(element instanceof HtmlElement)) return false;
+		HtmlElement htmlElement = (HtmlElement)element;
+		if (htmlElement.getElementType() == HtmlElementName.IMG && htmlElement.hasAttribute("src") && (!htmlElement.getAttribute("src").equals(""))) return true;
+		return false;
+	}
+	
 }

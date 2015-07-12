396f509f768beb53b9fe060c285bec00165d8bd6
Daisuke Kanno <daisuke6106@gmail.com>
2015-07-13 02:20:10 +0900

    URLとPATHのノード作成処理を追加

--- src/main/java/jp/co/dk/crawler/gdb/Url.java
+++ src/main/java/jp/co/dk/crawler/gdb/Url.java
@@ -18,11 +18,9 @@ public class Url extends AbstractUrl {
 	public boolean save() throws CrawlerSaveException {
 		try {
 			jp.co.dk.datastoremanager.gdb.AbstractDataBaseAccessObject dataStore = (jp.co.dk.datastoremanager.gdb.AbstractDataBaseAccessObject)this.dataStoreManager.getDataAccessObject("URL");
-			Cypher createData = new Cypher("CREATE(PAGE{url:{1})");
-			pageData.setParameter(this.url.toString());
-			pageData.setParameter(accessDateFormat.format(this.getCreateDate()));
-			pageData.setParameter(this.getData().getBytesToBase64String());
-			dataStore.execute(pageData);
+			Cypher createData = new Cypher("MERGE(:HOST{url:{1})").setParameter(this.getHost());
+			for (int i=0; i<this.getPathList().size() ;i++) createData.append("-[:PATH]->(:HOST{url:{").append(Integer.toString(i + 2)).append("})").setParameter(this.getPathList().get(i));
+			dataStore.execute(createData);
 		} catch (ClassCastException | DataStoreManagerException e) {
 			throw new CrawlerSaveException(DATASTOREMANAGER_CAN_NOT_CREATE);
 		}

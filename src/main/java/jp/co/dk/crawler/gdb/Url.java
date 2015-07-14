e75032ae6591f721ab82a93abf520e23cceb3aeb
Daisuke Kanno <daisuke6106@gmail.com>
2015-07-15 03:05:36 +0900

    URLのNeo4Jへの保存機能を追加

--- src/main/java/jp/co/dk/crawler/gdb/Url.java
+++ src/main/java/jp/co/dk/crawler/gdb/Url.java
@@ -1,12 +1,18 @@
 package jp.co.dk.crawler.gdb;
 
 import static jp.co.dk.crawler.message.CrawlerMessage.DATASTOREMANAGER_CAN_NOT_CREATE;
+import static jp.co.dk.datastoremanager.message.DataStoreManagerMessage.METHOD_TO_CONVERT_A_RESULT_IS_UNDEFINED;
+
+import java.util.Map;
+
 import jp.co.dk.browzer.exception.PageIllegalArgumentException;
 import jp.co.dk.crawler.AbstractUrl;
 import jp.co.dk.crawler.exception.CrawlerSaveException;
 import jp.co.dk.datastoremanager.DataStoreManager;
 import jp.co.dk.datastoremanager.exception.DataStoreManagerException;
 import jp.co.dk.datastoremanager.gdb.Cypher;
+import jp.co.dk.datastoremanager.gdb.DataBaseNode;
+import jp.co.dk.datastoremanager.gdb.DataConvertable;
 
 public class Url extends AbstractUrl {
 
@@ -18,14 +24,62 @@ public class Url extends AbstractUrl {
 	public boolean save() throws CrawlerSaveException {
 		try {
 			jp.co.dk.datastoremanager.gdb.AbstractDataBaseAccessObject dataStore = (jp.co.dk.datastoremanager.gdb.AbstractDataBaseAccessObject)this.dataStoreManager.getDataAccessObject("URL");
-			Cypher createData = new Cypher("MERGE(:HOST{url:{1})").setParameter(this.getHost());
-			for (int i=0; i<this.getPathList().size() ;i++) createData.append("-[:PATH]->(:HOST{url:{").append(Integer.toString(i + 2)).append("})").setParameter(this.getPathList().get(i));
-			dataStore.execute(createData);
+			
+			class CountComvertable implements DataConvertable {
+				private String countname;
+				private int    count;
+				CountComvertable(String countname){
+					this.countname = countname;
+				}
+				@Override
+				public DataConvertable convert(DataBaseNode dataBaseNode) throws DataStoreManagerException {
+					this.count = dataBaseNode.getInt(this.countname);
+					return this;
+				}
+				int getCount() {
+					return this.count;
+				}
+			}
+			
+			Cypher createData = new Cypher("MATCH(host:HOST{name:{1}}) RETURN COUNT(host)").setParameter(this.getHost());
+			CountComvertable hostCount = dataStore.executeWithRetuen(createData, new CountComvertable("COUNT(host)")).get(0);
+			if (hostCount.getCount() == 0) {
+				Cypher createHost = new Cypher("CREATE(host:HOST{name:{1}})").setParameter(this.getHost());
+				dataStore.execute(createHost);
+			}
+			
+			Cypher pathData = new Cypher("MATCH(host:HOST{name:{1}})").setParameter(this.getHost());
+			for (int i=0; i<this.getPathList().size(); i++) {
+				StringBuilder pathStr = new StringBuilder("-[:CHILD]->(path").append(i).append(":PATH{name:{").append(i+2).append("}})");  
+				Cypher clonedPathData = pathData.clone().append(pathStr.toString()).setParameter(this.getPathList().get(i));
+				CountComvertable pathCount = dataStore.executeWithRetuen(clonedPathData.clone().append("RETURN COUNT(*)"), new CountComvertable("COUNT(*)")).get(0);
+				if (pathCount.getCount() == 0) {
+					Cypher createPathData ;
+					if (i==0) {
+						createPathData = pathData.clone().append("CREATE(host)");
+					} else {
+						createPathData = pathData.clone().append("CREATE(path" + (i-1) + ")");
+					}
+					createPathData.append(pathStr.toString()).setParameter(this.getPathList().get(i));
+					dataStore.execute(createPathData);
+				}
+				pathData = clonedPathData;
+			}
+			
+			Cypher parameter = new Cypher("(");
+			Map<String, String> parameterMap = this.getParameter();
+			int index = 0;
+			for (Map.Entry<String, String> param : parameterMap.entrySet()) {
+				parameter.append(param.getKey()).append(":").append("{").append(Integer.toString(index)).append("},").setParameter(param.getValue());
+				index++;
+			}
+			
 		} catch (ClassCastException | DataStoreManagerException e) {
 			throw new CrawlerSaveException(DATASTOREMANAGER_CAN_NOT_CREATE);
 		}
 		
 		return false;
 	}
+	
 	
 }
--- src/test/java/jp/co/dk/crawler/gdb/UrlTest.java
+++ src/test/java/jp/co/dk/crawler/gdb/UrlTest.java
@@ -1,45 +1,229 @@
 package jp.co.dk.crawler.gdb;
 
 import static org.junit.Assert.*;
+import jp.co.dk.browzer.exception.PageAccessException;
 import jp.co.dk.browzer.exception.PageIllegalArgumentException;
+import jp.co.dk.crawler.exception.CrawlerSaveException;
 import jp.co.dk.crawler.gdb.Url;
 import jp.co.dk.crawler.rdb.CrawlerFoundationTest;
+import jp.co.dk.datastoremanager.DataStoreManager;
+import jp.co.dk.datastoremanager.exception.DataStoreManagerException;
+import jp.co.dk.document.exception.DocumentException;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.ResourceIterator;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class UrlTest extends CrawlerFoundationTest{
-
-	@Test
-	public void save() {
-		GraphDatabaseService graphDB = new GraphDatabaseFactory().newEmbeddedDatabase("/tmp/neo4j_sample");
-		Transaction tx = graphDB.beginTx();
-		try {
-			Url url1 = new Url("http://test.com", graphDB);
-			url1.save();
-			Url url2 = new Url("http://test.com/path1", graphDB);
-			url2.save();
-			Url url3 = new Url("http://test.com/path2", graphDB);
-			url3.save();
-			Url url4 = new Url("http://test.com/path1/path1-1", graphDB);
-			url4.save();
-			Url url5 = new Url("http://test.com/path1/path1-1/test.html", graphDB);
-			url5.save();
-			Url url6 = new Url("http://test.com/path1/path1-1/test.html?key1=value1", graphDB);
-			url6.save();
-			Url url7 = new Url("http://test.com/path1/path1-1/test.html?key1=value1&key2=value2", graphDB);
-			url7.save();
-			tx.success();
-		} catch (PageIllegalArgumentException e) {
-			fail(e);
-		} finally {
-			tx.finish();
-			graphDB.shutdown();
+	
+	public static class 正常にインスタンスが生成できた場合＿０１ extends CrawlerFoundationTest{
+		
+		protected Url sut;
+		
+		protected DataStoreManager dsm;
+		
+		@Before
+		public void init() throws DocumentException {
+			try {
+				this.dsm = getNeo4JAccessableDataStoreManager();
+				this.sut = new Url("http://test.com", dsm);
+			} catch (PageIllegalArgumentException e) {
+				fail(e);
+			}
+		}
+		
+		@Test
+		public void save() {
+			try {
+				this.dsm.startTrunsaction();
+				this.sut.save();
+				this.dsm.commit();
+			} catch (CrawlerSaveException e) {
+				fail(e);
+			} catch (DataStoreManagerException e) {
+				fail(e);
+			}finally {
+				try {
+					this.dsm.finishTrunsaction();
+				} catch (DataStoreManagerException e) {
+					fail(e);
+				}
+			}
 		}
 	}
+	
+	
+	public static class 正常にインスタンスが生成できた場合＿０２ extends CrawlerFoundationTest{
+		
+		protected Url sut;
+		
+		protected DataStoreManager dsm;
+		
+		@Before
+		public void init() throws DocumentException {
+			try {
+				this.dsm = getNeo4JAccessableDataStoreManager();
+				this.sut = new Url("http://test.com/test1", dsm);
+			} catch (PageIllegalArgumentException e) {
+				fail(e);
+			}
+		}
+		
+		@Test
+		public void save() {
+			try {
+				this.dsm.startTrunsaction();
+				this.sut.save();
+				this.dsm.commit();
+			} catch (CrawlerSaveException e) {
+				fail(e);
+			} catch (DataStoreManagerException e) {
+				fail(e);
+			}finally {
+				try {
+					this.dsm.finishTrunsaction();
+				} catch (DataStoreManagerException e) {
+					fail(e);
+				}
+			}
+		}
+	}
+	
+	public static class 正常にインスタンスが生成できた場合＿０３ extends CrawlerFoundationTest{
+		
+		protected Url sut;
+		
+		protected DataStoreManager dsm;
+		
+		@Before
+		public void init() throws DocumentException {
+			try {
+				this.dsm = getNeo4JAccessableDataStoreManager();
+				this.sut = new Url("http://test.com/test1/test1-1", dsm);
+			} catch (PageIllegalArgumentException e) {
+				fail(e);
+			}
+		}
+		
+		@Test
+		public void save() {
+			try {
+				this.dsm.startTrunsaction();
+				this.sut.save();
+				this.dsm.commit();
+			} catch (CrawlerSaveException e) {
+				fail(e);
+			} catch (DataStoreManagerException e) {
+				fail(e);
+			}finally {
+				try {
+					this.dsm.finishTrunsaction();
+				} catch (DataStoreManagerException e) {
+					fail(e);
+				}
+			}
+		}
+	}
+	
+	public static class 正常にインスタンスが生成できた場合＿０４ extends CrawlerFoundationTest{
+		
+		protected Url sut;
+		
+		protected DataStoreManager dsm;
+		
+		@Before
+		public void init() throws DocumentException {
+			try {
+				this.dsm = getNeo4JAccessableDataStoreManager();
+				this.sut = new Url("http://test.com/test1/test1-1/test1-1-1", dsm);
+			} catch (PageIllegalArgumentException e) {
+				fail(e);
+			}
+		}
+		
+		@Test
+		public void save() {
+			try {
+				this.dsm.startTrunsaction();
+				this.sut.save();
+				this.dsm.commit();
+			} catch (CrawlerSaveException e) {
+				fail(e);
+			} catch (DataStoreManagerException e) {
+				fail(e);
+			}finally {
+				try {
+					this.dsm.finishTrunsaction();
+				} catch (DataStoreManagerException e) {
+					fail(e);
+				}
+			}
+		}
+	}
+	
+public static class 正常にインスタンスが生成できた場合＿０５ extends CrawlerFoundationTest{
+		
+		protected Url sut;
+		
+		protected DataStoreManager dsm;
+		
+		@Before
+		public void init() throws DocumentException {
+			try {
+				this.dsm = getNeo4JAccessableDataStoreManager();
+				this.sut = new Url("http://test.com/test1/test1-1/test1-1-2", dsm);
+			} catch (PageIllegalArgumentException e) {
+				fail(e);
+			}
+		}
+		
+		@Test
+		public void save() {
+			try {
+				this.dsm.startTrunsaction();
+				this.sut.save();
+				this.dsm.commit();
+			} catch (CrawlerSaveException e) {
+				fail(e);
+			} catch (DataStoreManagerException e) {
+				fail(e);
+			}finally {
+				try {
+					this.dsm.finishTrunsaction();
+				} catch (DataStoreManagerException e) {
+					fail(e);
+				}
+			}
+		}
+	}
+//	@Test
+//	public void save() {
+//		GraphDatabaseService graphDB = new GraphDatabaseFactory().newEmbeddedDatabase("/tmp/neo4j_sample");
+//		Transaction tx = graphDB.beginTx();
+//		try {
+//			Url url1 = new Url("http://test.com", graphDB);
+//			url1.save();
+//			Url url2 = new Url("http://test.com/path1", graphDB);
+//			url2.save();
+//			Url url3 = new Url("http://test.com/path2", graphDB);
+//			url3.save();
+//			Url url4 = new Url("http://test.com/path1/path1-1", graphDB);
+//			url4.save();
+//			Url url5 = new Url("http://test.com/path1/path1-1/test.html", graphDB);
+//			url5.save();
+//			Url url6 = new Url("http://test.com/path1/path1-1/test.html?key1=value1", graphDB);
+//			url6.save();
+//			Url url7 = new Url("http://test.com/path1/path1-1/test.html?key1=value1&key2=value2", graphDB);
+//			url7.save();
+//			tx.success();
+//		} catch (PageIllegalArgumentException e) {
+//			fail(e);
+//		} finally {
+//			tx.finish();
+//			graphDB.shutdown();
+//		}
+//	}
 
 }

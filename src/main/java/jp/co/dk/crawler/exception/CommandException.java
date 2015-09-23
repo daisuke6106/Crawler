c29fed9cf35fa76a55610f9b4782d3daff3436f5
Daisuke Kanno <daisuke6106@gmail.com>
2015-09-23 23:44:47 +0900

    例外クラスにシリアルバージョンＩＤを追加

--- src/main/java/jp/co/dk/crawler/exception/CommandException.java
+++ src/main/java/jp/co/dk/crawler/exception/CommandException.java
@@ -2,6 +2,9 @@ package jp.co.dk.crawler.exception;
 
 public class CommandException extends RuntimeException {
 	
+	/**	シリアルバージョンID */
+	private static final long serialVersionUID = 2928831487428204617L;
+	
 	protected final int returnCode;
 	
 	public CommandException(int returnCode) {
--- src/main/java/jp/co/dk/crawler/exception/CommandFatalException.java
+++ src/main/java/jp/co/dk/crawler/exception/CommandFatalException.java
@@ -2,6 +2,9 @@ package jp.co.dk.crawler.exception;
 
 public class CommandFatalException extends Exception {
 	
+	/**	シリアルバージョンID */
+	private static final long serialVersionUID = -6899458810763234858L;
+	
 	protected final int returnCode;
 	
 	public CommandFatalException(int returnCode) {

73865bf8f3c964df086e060baf5aa71cbfd30a44
Daisuke Kanno <daisuke6106@gmail.com>
2015-12-16 01:40:08 +0900

    build.xmlのプロパティの修正 起動シェルの一部修正

--- bin/crawl.sh
+++ bin/crawl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CURRENT=$(cd $(dirname $0) && pwd)
-java -classpath "${CURRENT}/../*" jp.co.dk.crawler.controler.CrawlerMain $*
+java -classpath "${CURRENT}/../lib/*:${CURRENT}/../classes" jp.co.dk.crawler.gdb.controler.CrawlerMain $*
 

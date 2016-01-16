c69192e5942e5febf1f298aae6f68584b2a82aa2
Daisuke Kanno <daisuke6106@gmail.com>
2016-01-16 23:24:44 +0900

    クローリング対象を開始ホストと同じホストのみにするメソッドを追加

--- src/main/java/jp/co/dk/crawler/gdb/controler/CrawlerSaveMain.java
+++ src/main/java/jp/co/dk/crawler/gdb/controler/CrawlerSaveMain.java
@@ -1,6 +1,9 @@
 package jp.co.dk.crawler.gdb.controler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import jp.co.dk.browzer.exception.PageAccessException;
 import jp.co.dk.browzer.exception.PageIllegalArgumentException;
@@ -8,8 +11,10 @@ import jp.co.dk.browzer.exception.PageMovableLimitException;
 import jp.co.dk.browzer.exception.PageRedirectException;
 import jp.co.dk.crawler.controler.AbtractCrawlerControler;
 import jp.co.dk.crawler.exception.CrawlerInitException;
+import jp.co.dk.crawler.exception.CrawlerReadException;
 import jp.co.dk.crawler.exception.CrawlerSaveException;
 import jp.co.dk.crawler.gdb.GCrawler;
+import jp.co.dk.crawler.gdb.GUrl;
 import jp.co.dk.document.exception.DocumentException;
 import jp.co.dk.neo4jdatastoremanager.Neo4JDataStoreManager;
 import jp.co.dk.neo4jdatastoremanager.exception.Neo4JDataStoreManagerCypherException;
@@ -29,10 +34,35 @@ public class CrawlerSaveMain extends AbtractCrawlerControler {
 			dsm.startTrunsaction();
 			GCrawler crawler = new GCrawler(cmd.getOptionValue("url"), dsm);
 			if (cmd.hasOption("all")) {
-				crawler.saveAll();
-				crawler.saveAllUrl();
+				while(true) {
+					crawler.saveAll();
+	 				crawler.saveAllUrl();
+	 				
+	 				GUrl url = (GUrl)crawler.getPage().getUrl();
+	 				
+	 				class TmpUrl {
+	 					private int savedCount;
+	 					private GUrl url;
+	 					TmpUrl(GUrl url) throws CrawlerReadException {
+	 						this.url        = url;
+	 						this.savedCount = url.getSavedCount();
+	 					}
+	 				}
+	 				
+	 				List<TmpUrl> tmpUrlList = new ArrayList<TmpUrl>();
+	 				List<GUrl> sameHostUrlList = url.getAllUrlBySameHost();
+	 				for (GUrl sameHostUrl : sameHostUrlList) tmpUrlList.add(new TmpUrl(sameHostUrl));
+	 				tmpUrlList.sort(new Comparator<TmpUrl>() {
+						@Override
+						public int compare(TmpUrl o1, TmpUrl o2) {
+							return o1.savedCount - o2.savedCount;
+						}
+					});
+	 				crawler = new GCrawler(tmpUrlList.get(0).url.toString(), dsm);
+	 				Thread.sleep(1 * 1000);
+				}
 			} else {
- 				crawler.save();
+ 				crawler.saveAll();
  				crawler.saveAllUrl();
 			}
 			System.exit(0);
@@ -61,6 +91,12 @@ public class CrawlerSaveMain extends AbtractCrawlerControler {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		} catch (CrawlerReadException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		} catch (InterruptedException e) {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		} catch (RuntimeException e) {

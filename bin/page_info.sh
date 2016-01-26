46cc3582bec820b92008618815f5319abbbebdfb
Daisuke Kanno <daisuke6106@gmail.com>
2016-01-27 02:35:04 +0900

    スクリプトの参照jarファイルを修正

--- bin/page_info.sh
+++ bin/page_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CURRENT=$(cd $(dirname $0) && pwd)
-MAIN_JAR="${CURRENT}/../crawler_0.1.0.jar"
+MAIN_JAR="${CURRENT}/../crawler_0.1.0a.jar"
 LIB_JAR="${CURRENT}/../lib/*"
 MESSAGE_DIR="${CURRENT}/../messages"
 PROPERTY_DIR="${CURRENT}/../properties"

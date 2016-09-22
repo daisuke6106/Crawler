cab4e9eda1abaad007e09b351bff9dfe89747208
Daisuke Kanno <daisuke6106@gmail.com>
2016-09-22 13:19:08 +0900

    シェルスクリプトの構成変更

--- bin/page_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CURRENT=$(cd $(dirname $0) && pwd)
-MAIN_JAR="${CURRENT}/../crawler_0.1.1a.jar"
-LIB_JAR="${CURRENT}/../lib/*"
-MESSAGE_DIR="${CURRENT}/../messages"
-PROPERTY_DIR="${CURRENT}/../properties"
-java -classpath "${MAIN_JAR}:${LIB_JAR}:${MESSAGE_DIR}:${PROPERTY_DIR}" jp.co.dk.crawler.controler.PageInfoMain $*
-
